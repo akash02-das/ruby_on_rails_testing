a48abf9a9ea5dc5ac019e9f9bf65da9f346c93ad
akash <aku.das02@gmail.com>
2021-05-18 01:15:04 +0600

    created a signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'registrations#new'
+  post 'signup', to: 'registrations#create'
   get 'about-us', to: 'about#index', as: 'about'
   root to: 'home#index'
 end
