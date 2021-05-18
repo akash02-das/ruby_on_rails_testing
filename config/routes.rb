d6a7223c91b0be0df24ae551bab8323c08cb68bc
akash <aku.das02@gmail.com>
2021-05-18 08:47:45 +0600

    user logged out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'signup', to: 'registrations#new'
   post 'signup', to: 'registrations#create'
+  delete 'logout', to: 'sessions#destroy'
   get 'about-us', to: 'about#index', as: 'about'
   root to: 'home#index'
 end
