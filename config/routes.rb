5a96b57e0b1d9bb9a3867edaf00dc6a720ebd7c5
akash <aku.das02@gmail.com>
2021-05-18 09:18:08 +0600

    login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'signup', to: 'registrations#new'
   post 'signup', to: 'registrations#create'
+
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+
   delete 'logout', to: 'sessions#destroy'
+
   get 'about-us', to: 'about#index', as: 'about'
   root to: 'home#index'
 end
